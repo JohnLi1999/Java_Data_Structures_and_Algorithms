bc38c53b509f56c502fe2d442895cd71b1321cad
zhiwenli <lizhiwenzg@gmail.com>
2020-08-05 16:32:47 -0700

    Update Disjoint Set

--- src/Disjoint_Set/DisjointSet.java
+++ src/Disjoint_Set/DisjointSet.java
@@ -2,68 +2,90 @@ package Disjoint_Set;
 
 public class DisjointSet {
 
-    private int[] roots;
+    private final int[] parent;
+    private final int[] rank;
 
+    /** Initialize the disjoint set
+     *
+     * @param size  the size of the disjoint set
+     */
     public DisjointSet(int size) {
-        roots = new int[size];
+        parent = new int[size];
+        rank = new int[size];
 
         // initialize the root as the element itself
         for (int i = 0; i < size; i++) {
-            roots[i] = i;
+            parent[i] = i;
         }
     }
 
     /** Find the root of the specified element
+     *  We also apply Path Compression:
+     *      Path compression flattens the structure of the tree by
+     *      making every node point to the root whenever Find is used on it
+     *
+     *  Time Complexity: O(M * α(N)) on average
+     *      Where M is the number of operations applied, and α(N) is the amortized time and α(N) < 5
+     *      So, "Find" take place in essentially constant time
      *
      * @param i     the specified element
      * @return      the root of the element
      */
-    public int findRoot(int i) {
-        int root = i;
-
-        /*  Find the root of the specified element
-            The root of an root element points to itself  */
-        while (root != roots[root]) {
-            root = roots[root];
+    public int find(int i) {
+        // Recursive Approach
+        if (parent[i] != i) {
+            parent[i] = find(parent[i]);
         }
+        return parent[i];
 
-        /*  Optimization:
-            1. To reduce the depth of a disjoint set, we want all the elements in the same set
-            share the same root element. So the depth of the set is always one
-            2. So we have another while loop staring from the specified element (i) to its root (root),
-            for every element in between (include the starting element), we update their root element to
-            the root we found before  */
-        while (i != roots[i]) {
-            int temp = roots[i];
-            roots[i] = root;
-            i = temp;
+        // Iteration Approach
+//        int root = i;
+//        while (parent[root] != root) {
+//            root = parent[root];
+//        }
+//        while (parent[i] != i) {
+//            int temp = parent[i];
+//            parent[i] = root;
+//            i = temp;
+//        }
+//        return root;
+    }
+
+    /** Union the two elements into the same set
+     *  We also apply union-by-rank
+     *
+     *  Time Complexity: O(M * α(N)) on average
+     *      Where M is the number of operations applied, and α(N) is the amortized time and α(N) < 5
+     *      So, "Union" take place in essentially constant time
+     *
+     * @param x     the first element
+     * @param y     the second element
+     */
+    public void union(int x, int y) {
+        int xRoot = find(x);
+        int yRoot = find(y);
+
+        if (xRoot != yRoot) {
+            if (rank[xRoot] < rank[yRoot]) {
+                parent[xRoot] = yRoot;
+            } else if (rank[xRoot] > rank[yRoot]) {
+                parent[yRoot] = xRoot;
+            } else {
+                parent[yRoot] = xRoot;
+                rank[xRoot]++;
+            }
         }
-
-        return root;
     }
 
     /** Check if the two elements are in the same set
      *
-     * @param p     the first element
-     * @param q     the second element
+     * @param x     the first element
+     * @param y     the second element
      * @return      true if the two elements are not in the same set,
      *              false otherwise
      */
-    public boolean connected(int p, int q) {
+    public boolean connected(int x, int y) {
         // If the two elements are in the same set, their roots should be the same
-        return findRoot(p) == findRoot(q);
-    }
-
-    /** Union the two elements into the same set
-     *
-     * @param p     the first element
-     * @param q     the second element
-     */
-    public void union(int p, int q) {
-        int pRoot = findRoot(p);
-        int qRoot = findRoot(q);
-
-        // To union two elements, we only need to point the root of one element to the root of the other element
-        roots[pRoot] = qRoot;
+        return find(x) == find(y);
     }
 }
